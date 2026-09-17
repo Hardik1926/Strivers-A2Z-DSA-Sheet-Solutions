f0221738dacf7c82f9239df0ccbf0d8504e17865
Hardik <hardikballah@gmail.com>
2026-09-17 21:02:51 +0530

    did not solve easy08

---
+++ 01.Arrays/1.Easy/easy08.java
@@ -0,0 +1,14 @@
+/*
+Question:
+Given an array, and an element num the task is to find if num is present in the given array or not. If present print the index of the element or print -1.
+ */
+
+public class easy08 {
+    
+    public static void main(String[] args) {
+        /*  The question is way to easy
+            I don't think anyone needs it's solution. The idea is to traverse the array using loop and when the element
+        i   s equal to k return the same
+        */
+    }
+}
